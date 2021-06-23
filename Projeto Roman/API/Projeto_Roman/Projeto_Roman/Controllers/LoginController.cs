b7dc800d14e358c3ae965250627a392e1ccbdf45
Robert Sena Santos <40228034876@info.local>
2021-06-23 14:56:45 -0300

    JWT Finalizada

--- Projeto Roman/API/Projeto_Roman/Projeto_Roman/Controllers/LoginController.cs
+++ Projeto Roman/API/Projeto_Roman/Projeto_Roman/Controllers/LoginController.cs
@@ -1,6 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Projeto_Roman.Domains;
+using Projeto_Roman.Interfaces;
+using Projeto_Roman.Repository;
+using Projeto_Roman.ViewModel;
+using System.IdentityModel.Tokens.Jwt;
 using System;
+using System.Security.Claims;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,13 +19,69 @@ namespace Projeto_Roman.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
-        try
+        private IUsuarioRepository _usuarioRepository { get; set; }
+
+        public LoginController()
         {
-            
+            _usuarioRepository = new UsuarioRepository();
         }
-        catch (ex)
+
+        /// <summary>
+        /// faz o login de um usuário
+        /// </summary>
+        /// <param name="login">Objeto com as informações de login</param>
+        /// <returns>Um status code 200 - OK com o token</returns>
+        [HttpPost]
+        public IActionResult Post(LoginViewModel login)
         {
-             // TODO
+            try
+            {
+                Usuario usuarioBuscado = _usuarioRepository.Login(login.Email, login.Senha);
+
+                if (usuarioBuscado == null)
+                {
+                    return NotFound("E-mail ou senha inválidos!");
+                }
+
+                //Define as informações que serão armazenadas no token
+                var claims = new[]
+                {
+                    //Email do usuario autenticado
+                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+
+                    //Id do usuario autenticado
+                    new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.IdUsuario.ToString()),
+
+                    //O tipo de usuario autenticado, definindo ele como parâmetro para o AUTHORIZE
+                    new Claim(ClaimTypes.Role, usuarioBuscado.IdTipoUsuario.ToString())
+                };
+
+                //Chave de acesso ao token
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("projeto-roman-autenticacao"));
+
+                //Credencias do token - header
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                //Token
+                var token = new JwtSecurityToken(
+
+                    issuer : "projetoRoman.webApi",      //Emissor do token
+                    audience : "projetoRoman.webApi",    //Destinatário do token
+                    claims : claims,                            //Dados da claim
+                    expires : DateTime.Now.AddMinutes(60),      //Tempo de expiração
+                    signingCredentials : creds                  //Credenciais do token
+                );
+
+                //Retorna OK com o token
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
     }
 }
--- Projeto Roman/API/Projeto_Roman/Projeto_Roman/Startup.cs
+++ Projeto Roman/API/Projeto_Roman/Projeto_Roman/Startup.cs
@@ -3,10 +3,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Projeto_Roman
@@ -17,8 +21,15 @@ namespace Projeto_Roman
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-
+            services.AddControllers()
+                    .AddNewtonsoftJson(options =>
+                    {
+                        //Ignora os loopings nas consultas
+                        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                        //Ignora os valores nulos ao fazer junções nas consultas
+                        options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    });
+            
             services.AddSwaggerGen(c=>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
@@ -27,6 +38,40 @@ namespace Projeto_Roman
                     Version = "v1"
                 });
             });
+
+            services
+                .AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = "JwtBearer";
+                    x.DefaultChallengeScheme = "JwtBearer";
+                })
+
+                .AddJwtBearer("JwtBearer", x =>
+                {
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        //Define que o issuer será validado
+                        ValidateIssuer = true,
+
+                        //Define que o audience será validado
+                        ValidateAudience = true,
+
+                        //Define que o tempo de vida será validado
+                        ValidateLifetime = true,
+
+                        //Forma de criptografia e a chave de autenticação
+                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("projeto-roman-autenticacao")),
+
+                        //Verifica o tempo de expiração do token
+                        ClockSkew = TimeSpan.FromMinutes(60),
+
+                        //Define de onde está vindo (NOME ISSUER)
+                        ValidIssuer = "projetoRoman.webApi",
+
+                        //Define para onde vai (NOME AUDIENCE)
+                        ValidAudience = "projetoRoman.webApi"
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,18 +84,24 @@ namespace Projeto_Roman
 
             app.UseRouting();
 
-            app.UseSwagger();
+            //Habilita a autenticação
+            app.UseAuthentication();
 
-            app.UseSwaggerUI(c =>
-           {
-               c.SwaggerEndpoint("/swagger/v1/swagger.json", "Roman.webAPI")
-
-               c.RoutePrefix = string.Empty;
-           });
+            //Habilita a autorização
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+               c.SwaggerEndpoint("/swagger/v1/swagger.json", "Roman.webAPI");
+
+               c.RoutePrefix = string.Empty;
             });
         }
     }
